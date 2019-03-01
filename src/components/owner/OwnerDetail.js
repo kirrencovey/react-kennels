5723f9dd4c073974d55c63055bf452effdc72fe6
Kirren Covey <kirrencovey@gmail.com>
2019-03-01 15:42:16 -0600

    fixed styling

--- src/components/owner/OwnerDetail.js
+++ src/components/owner/OwnerDetail.js
@@ -6,7 +6,7 @@ export default class OwnerDetail extends Component {
         const owner = this.props.owners.find(a => a.id === parseInt(this.props.match.params.ownerId)) || {}
 
         return (
-            <section className="owner">
+            <section>
                 <div key={owner.id} className="card">
                     <div className="card-body">
                         <h4 className="card-title">
